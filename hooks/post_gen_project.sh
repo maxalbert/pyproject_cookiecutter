d73f03d00b11ef3e7f67587ec758c69bfc196938
Maximilian Albert <maximilian.albert@flowminder.org>
2020-02-07 19:50:43 +0000

    Print empty lines explicitly

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,23 +3,28 @@
 set -euo pipefail
 
 # Put the project under version control
-echo "\nPutting project under version control ..."
+echo ""
+echo "Putting project under version control ..."
 git init
 git add .
 git commit -a -m "Initial commit"
 
 # Install project dependencies (but not the project itself)
-echo "\nInstalling project dependencies (in a new poetry virtual environment) ..."
+echo ""
+echo "Installing project dependencies (in a new poetry virtual environment) ..."
 poetry install --no-root
 
 # Initialise version management
-echo "\nInitialising version management using versioneer ..."
+echo ""
+echo "Initialising version management using versioneer ..."
 poetry run versioneer install
 git add .
 git commit -a -m "Initialise version management"
 
 # Tag git repo with initial version
-echo "\nTagging git repo with initial version '{{ cookiecutter.project_version }}'"
+echo ""
+echo "Tagging git repo with initial version '{{ cookiecutter.project_version }}'"
 git tag -f {{ cookiecutter.project_version }}
 
-echo "\nDone. All tasks completed successfully."
+echo ""
+echo "Done. All tasks completed successfully."
