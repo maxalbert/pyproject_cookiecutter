73fdeb3dc466a237f13e1a7c2bfff93b89189402
Maximilian Albert <maximilian.albert@flowminder.org>
2020-02-08 23:44:32 +0000

    Add dummy module and function so that we can have more explicit and less flaky tests

--- {{cookiecutter.pymodule_name}}/tests/test_{{cookiecutter.pymodule_name}}.py
+++ {{cookiecutter.pymodule_name}}/tests/test_{{cookiecutter.pymodule_name}}.py
@@ -1,6 +1,8 @@
 from .context import {{ cookiecutter.pymodule_name }}
-from {{ cookiecutter.pymodule_name }} import __version__
+from {{ cookiecutter.pymodule_name }} import dummy_function
 
 
-def test_version():
-    assert __version__.startswith("{{ cookiecutter.project_version   }}")
+def test_dummy_function():
+    assert 4 == dummy_function(2)
+    assert 9 == dummy_function(3)
+    assert 16 == dummy_function(4)
--- {{cookiecutter.pymodule_name}}/{{cookiecutter.pymodule_name}}/__init__.py
+++ {{cookiecutter.pymodule_name}}/{{cookiecutter.pymodule_name}}/__init__.py
@@ -1,1 +1,3 @@
 # __init__.py file for `{{ cookiecutter.pymodule_name }}`
+
+from .dummy import dummy_function
